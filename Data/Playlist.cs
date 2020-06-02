b8f2d767ba104956e41485e8229740daea444148
Emina Palamarevic <minapala342@gmail.com>
2020-06-01 20:25:51 -0400

    playlist updating

--- Data/Playlist.cs
+++ Data/Playlist.cs
@@ -14,20 +14,20 @@ namespace Data
         public int PlaylistId { get; set; }
         public string Title {get; set;}
 
+        
         public virtual ICollection<Track> Tracks { get; set; }
+        
 
-        public int TrackId { get; set; }
-        public virtual Track Track { get; set; }
         public bool IsActive { get; set; } = true;
 
-        public int NumberOfPlaylistTracks
+        public int NumberOfTracks
         {
             get
             {
                 using (var ctx = new ApplicationDbContext())
                 {
-                    var tracks = ctx.Tracks.Where(e => e.TrackId == TrackId && e.IsActive == true).Count();
-                    return tracks;
+                    var Tracks = ctx.Tracks.Where(e => e.TrackId == PlaylistId && e.IsActive == true).Count();
+                    return Tracks;
                 }
             }
         }
